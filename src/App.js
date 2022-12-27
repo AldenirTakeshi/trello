a9bcc3300a7654c3b7f5581c87da38dfa1eca77e
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2022-12-27 09:38:20 -0400

    teste lib

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
+const inicialItems = [
+  { id: "1", content: "Conteudo 1" },
+  { id: "2", content: "Conteudo 2" },
+  { id: "3", content: "Conteudo 3" },
+];
+const inicialColumns = [{ name: "To do", id: "123", items: inicialItems }];
 function App() {
+  const [columns, setColumns] = useState(inicialColumns);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Trello Clone</p>
-      </header>
+      {columns.map((column) => (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <h2>{column.name}</h2>
+          <div style={{ backgroundColor: "lightBlue", height: 500, width: 250 }}>
+            {column.items.map((item) => (
+              <div
+                style={{
+                  backgroundColor: "gray",
+                  padding: ".8rem",
+                  margin: ".8rem",
+                }}
+              >
+                {item.content}
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
